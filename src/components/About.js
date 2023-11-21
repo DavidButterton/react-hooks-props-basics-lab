5b7c6eea56903dcf868cd4912b1870c44116faa7
DavidButterton <sadhu.butterton@gmail.com>
2023-11-21 11:50:44 -0600

    assignment completion

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {  
+//if props of bio is passed to this component, it is displayed inside of a <p> tag
+//if props of bios is not passed down, or if bio is an empty string, don't display the <p> tag
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio  ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links linkedin={props.links.linkedin} github={props.links.github}/>
+    
     </div>
   );
 }
